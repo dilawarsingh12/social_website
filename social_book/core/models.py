7d1c34f93ebcfb85d99cda3606064cf92a7a3df4
dilawarsingh12 <dsingh_be19@thapar.edu>
2023-05-15 00:00:45 +0530

    uploading post

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,16 +1,45 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+import uuid
+from datetime import datetime
 
 User=get_user_model()
 # Create your models here.
+
+def user_directory_path_profile(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<username>/<filename>
+    return 'profile/user_{0}/{1}'.format(instance.user.username, filename)
+
+def user_directory_post_profile(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<username>/<filename>
+    return 'post/user_{0}/{1}'.format(instance.user, filename)
+
 
 class Profile(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     id_user=models.IntegerField()
     bio=models.TextField(blank=True)
-    profileimg= models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    profileimg= models.ImageField(upload_to=user_directory_path_profile, default='blank-profile-picture.png')
     location=models.CharField(max_length=100,blank=True)
 
 
     def __str__(self):
         return self.user.username 
+
+
+class Post(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4)
+    user=models.CharField(max_length=100)
+    image=models.ImageField(upload_to=user_directory_post_profile)
+    caption=models.TextField()
+    created_at=models.DateTimeField(default=datetime.now)
+    no_of_likes=models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.user
+
+# superuser = User.objects.filter(is_superuser=True).first()
+# if superuser:
+#     Profile.objects.get_or_create(user=superuser)
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,11 +1,117 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import auth , User
+from .models import Profile,Post
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
-    return render(request,'index.html')
+    user_object=User.objects.get(username=request.user.username)
+    user_profile=Profile.objects.get(user=user_object)
+
+    posts=Post.objects.all()
+    return render(request,'index.html',{'user_profile':user_profile,'posts':posts})
+
+
+
+@login_required(login_url='signin')
+def upload (request):
+
+    if request.method=='POST':
+        user=request.user.username
+        image=request.FILES.get('image_upload')
+        caption=request.POST['caption']
+
+        new_post=Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+        
+        return redirect('/')
+    return  redirect('/')
+
+
+@login_required(login_url='signin')
+def settings (request):
+    user_profile=Profile.objects.get(user=request.user)
+    if request.method == "POST" :
+        if request.FILES.get('image') == None:
+            image=user_profile.profileimg
+            bio=request.POST['bio']
+            location=request.POST['location']
+
+            user_profile.profileimg=image
+            user_profile.bio=bio
+            user_profile.location=location
+            user_profile.save()
+        else:
+            image=request.FILES.get('image')
+            bio=request.POST['bio']
+            location=request.POST['location']
+
+            user_profile.profileimg=image
+            user_profile.bio=bio
+            user_profile.location =location
+            user_profile.save()
+        return redirect('settings')
+    return render(request,'setting.html',{'user_profile':user_profile})
 
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == "POST":
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+
+        if password==password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'username taken')
+                return redirect('signup')
+            else:
+                user=User.objects.create_user(username=username, email=email,password=password)
+                user.save()
+
+                user_login=auth.authenticate(username=username,password=password)
+                auth.login(request,user_login)
+
+                # lets create profile model for the new user
+                user_model=User.objects.get(username=username)
+                new_profile=Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
+        else:
+            messages.info(request,'password are not  matching')
+            return redirect('signup')
+    else:
+        return render(request,'signup.html')
+
+
+
+def signin(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        elif auth.authenticate(username=username) is not None:
+            messages.info(request,'password is wrong')
+            return redirect('signin')
+        else:
+            messages.info(request,'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request,'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect ('signin')
